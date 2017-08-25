2a326cfdd0dc7931c78c24d16d092fd46c77a52b
Bart Kampers <github@bartkampers.nl>
2017-08-25 15:24:20 +0200

    Interface

--- src/bka/uml/Interface.java
+++ src/bka/uml/Interface.java
@@ -7,30 +7,8 @@ package bka.uml;
 import java.util.*;
 
 
-public class Interface extends bka.graph.Vertex implements Type, bka.ocl.Named {
+public interface Interface extends Type {
 
-    
-    public Interface() {    
-    }
+    public List<Operation> getOperations();
 
-
-    @Override
-    public boolean isAbstract() {
-        return true;
-    }
-   
-    
-    public List<Operation> getOperations() {
-        return (operations != null) ? new ArrayList<>(operations) : null;
-    }
-    
-    
-    public void setOperations(List<Operation> operations) {
-        this.operations = (operations != null) ? new ArrayList<>(operations) : null;
-    }
-    
-    
-    private List<Operation> operations;
-
-    
 }
